fd2f4589b2dc19ce5403978cdcda979c7c12ee22
raych2 <rtongco@gmail.com>
2021-10-28 20:18:12 -0700

    Install react-router-dom and add Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from 'react';
+
+const App = () => {
+  return <div className="App">Blog Posts</div>;
+};
 
 export default App;
---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import App from './App';
+
+const Routes = () => {
+  return (
+    <Router basename="/">
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
