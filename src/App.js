34e4be77578e5a3bf95c2002043d596123c4b870
raych2 <rtongco@gmail.com>
2021-11-03 21:16:39 -0700

    Fetch data from blog api and render post list with PostPreview

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import PostPreview from './components/PostPreview';
 
 const App = () => {
-  return <div className="App">Blog Posts</div>;
+  const [blogPosts, setBlogPosts] = useState([]);
+  const [error, setError] = useState(false);
+  const blogAPI = 'https://rt-blog-api.herokuapp.com/posts';
+
+  useEffect(() => {
+    async function fetchBlogAPI() {
+      setError(false);
+      try {
+        const response = await fetch(blogAPI);
+        const data = await response.json();
+        setBlogPosts(data.posts);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+      }
+    }
+
+    fetchBlogAPI();
+  }, []);
+
+  return (
+    <div>
+      {error && <div>An error occurred</div>}
+      <div>
+        {blogPosts.map((post, index) => {
+          return (
+            <div key={post._id}>
+              <PostPreview post={blogPosts[index]} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default App;
