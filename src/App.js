fca75d8690141d64bcd1bb7c9593c3efa372bf30
raych2 <rtongco@gmail.com>
2021-11-04 21:18:41 -0700

    Add spinner to indicate loading api data

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import PostPreview from './components/PostPreview';
+import Spinner from 'react-spinkit';
+
+const SpinnerContainer = styled.div`
+  margin-top: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`;
 
 const App = () => {
   const [blogPosts, setBlogPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const blogAPI = 'https://rt-blog-api.herokuapp.com/posts';
 
   useEffect(() => {
     async function fetchBlogAPI() {
       setError(false);
+      setLoading(true);
       try {
         const response = await fetch(blogAPI);
         const data = await response.json();
@@ -19,6 +31,7 @@ const App = () => {
         console.log(err);
         setError(true);
       }
+      setLoading(false);
     }
 
     fetchBlogAPI();
@@ -27,15 +40,21 @@ const App = () => {
   return (
     <div>
       {error && <div>An error occurred</div>}
-      <div>
-        {blogPosts.map((post, index) => {
-          return (
-            <div key={post._id}>
-              <PostPreview post={blogPosts[index]} />
-            </div>
-          );
-        })}
-      </div>
+      {loading ? (
+        <SpinnerContainer>
+          <Spinner name="ball-spin-fade-loader" color="#FCA311" />
+        </SpinnerContainer>
+      ) : (
+        <div>
+          {blogPosts.map((post, index) => {
+            return (
+              <div key={post._id}>
+                <PostPreview post={blogPosts[index]} />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
