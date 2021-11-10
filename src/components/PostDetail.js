73c80f5d66297d016c9215c3fcfa109b77e6817b
raych2 <rtongco@gmail.com>
2021-11-09 22:06:41 -0800

    Add CommentForm and addComment method to PostDetail

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -28,6 +28,42 @@ const PostSection = styled.div`
 const CommentSection = styled.div`
   padding: 20px;
 `;
+const CommentForm = styled.form`
+  display: flex;
+  flex-flow: column wrap;
+  margin: 10px auto 0 auto;
+  padding: 20px;
+`;
+const GuestName = styled.input`
+  display: block;
+  width: 100%;
+  height: 40px;
+  margin: 10px auto;
+  font-size: 1em;
+  padding-left: 5px;
+  border: 0.5px solid #e5e5e5;
+  border-radius: 5px;
+`;
+const GuestComment = styled.textarea`
+  display: block;
+  width: 100%;
+  margin: 10px auto;
+  font-size: 1em;
+  padding: 10px 0 0 5px;
+  border: 0.5px solid #e5e5e5;
+  border-radius: 5px;
+`;
+const SubmitButton = styled.input`
+  margin: 5px auto;
+  width: 25%;
+  padding: 10px;
+  text-align: center;
+  color: #e8eddf;
+  background-color: #242423;
+  border-color: #242423;
+  border-radius: 3px;
+  font: 100 1rem 'Noto Sans JP', sans-serif;
+`;
 const CommentList = styled.div`
   padding: 20px;
 `;
@@ -47,11 +83,15 @@ const Text = styled.p`
 const CommentHeader = styled.h2`
   margin: 5px auto;
 `;
+
 const PostDetail = ({ match }) => {
   const [blogPost, setBlogPost] = useState([]);
   const [author, setAuthor] = useState('');
+  const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [guestName, setGuestName] = useState('');
+  const [guestComment, setGuestComment] = useState('');
 
   useEffect(() => {
     async function fetchBlogPostAPI() {
@@ -64,6 +104,7 @@ const PostDetail = ({ match }) => {
         const data = await response.json();
         setBlogPost(data.post);
         setAuthor(data.post.author.username);
+        setComments(data.post.comments);
       } catch (err) {
         console.log(err);
         setError(true);
@@ -73,6 +114,30 @@ const PostDetail = ({ match }) => {
 
     fetchBlogPostAPI();
   }, [match.params.id]);
+
+  const addComment = async(e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const response = await fetch(`https://rt-blog-api.herokuapp.com/posts/${match.params.id}/comments/addComment`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          author: guestName, 
+          text: guestComment
+        }),
+      });
+      const commentData = await response.json();
+      setComments([...comments, commentData.comment]);
+    } catch (err) {
+      console.log(err)
+      setError(true);
+    }
+    setGuestName('');
+    setGuestComment('');
+  }
 
   return (
     <div>
@@ -91,9 +156,32 @@ const PostDetail = ({ match }) => {
           </PostSection>
           <CommentSection>
             <CommentHeader>Comments:</CommentHeader>
+              <CommentForm onSubmit={addComment}>
+                <label htmlFor="guestName">Name:</label>
+                <GuestName
+                  value={guestName}
+                  type="text"
+                  id="guestName"
+                  onChange={(e) => setGuestName(e.target.value)}
+                  placeholder="Name"
+                  required
+                />
+                <label htmlFor="guestComment">Comment:</label>
+                <GuestComment
+                  value={guestComment}
+                  id="guestComment"
+                  rows="3"
+                  cols="33"
+                  maxLength="500"
+                  onChange={(e) => setGuestComment(e.target.value)}
+                  placeholder="Share your thoughts on this blog post"
+                  required
+                />
+                <SubmitButton type="submit" value="Submit" />
+              </CommentForm>
             <CommentList>
-              {blogPost.comments && blogPost.comments.length > 0 ? (
-                blogPost.comments.map((comment) => {
+              {comments && comments.length > 0 ? (
+                comments.map((comment) => {
                   return (
                     <div key={comment._id}>
                       <CommentDetail comment={comment} />
