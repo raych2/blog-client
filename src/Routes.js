fd76a11b48fff8099e6c02cf994fc7d9b99df721
raych2 <rtongco@gmail.com>
2021-11-23 21:29:19 -0800

    Redirect to home page if the user navigates to /posts

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import App from './App';
 import Header from './components/Header';
 import PostDetail from './components/PostDetail';
@@ -10,6 +10,7 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/posts/:id" component={PostDetail} />
+        <Redirect exact from="/posts" to="/" />
       </Switch>
     </Router>
   );
